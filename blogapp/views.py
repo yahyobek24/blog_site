12425459ea52da6675b3aa73acf30c9f4a87e498
user <yahyobek@gmail.ru>
2025-01-08 18:06:41 +0500

    commit models, views, templates

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,19 @@
+from gc import get_objects
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+from django.views.generic import ListView
+
+class PstListWiews(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = "blog/post/list.html"
+
+
+
+
+def post_detail(request, year, month, day, slug, get_objects_or_404=None):
+    post = get_objects_or_404(Post, slug=slug, status= "published", publish__yead=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post' : post})
